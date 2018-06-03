adf0eeeac0d389d08a81ae7c8f8761e39f314314
thorzhou <thorzhou@hotmail.com>
2018-06-03 11:38:42 +0800

    reg done

--- practice/p2/mafia/main.js
+++ practice/p2/mafia/main.js
@@ -4,15 +4,27 @@ function setNumOfPlayer(){
     var num = document.getElementById('playerSetting').value;
     var killer = document.getElementById('numOfKiller');
     var civilian = document.getElementById('numOfCivilian');
+    playerIdentityList.splice(0,playerIdentityList.length);
+    var reg = /^\d{1,2}$/;
+    num = num.replace(/\s/g, "");
+    if (reg.test(num)) {
+        if(num < 4 || num > 18)
+        {
+            alert('请输入正确的玩家人数');
+            return;
+        }
+    }else{
+        alert('请输入正确的玩家人数');
+        return;
+    }
     var kn = Math.floor(num/3);
     killer.innerHTML = '杀手'+ kn + ' 人';
     civilian.innerHTML = '平民' + (num-kn) + ' 人';
     resetIdentity(num,kn);
-    console.log(playerIdentityList)
+    console.log(playerIdentityList);
 }
 
 function resetIdentity(len,kn){
-    playerIdentityList.splice(0,playerIdentityList.length);
     for (let index = 0; index < len; index++) {
         if(index<kn){
             playerIdentityList[index] = 'Killer';
