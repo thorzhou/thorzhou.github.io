ced985c901e391a1ed70c6c3e01c3d7c23295169
thorzhou <thorzhou@hotmail.com>
2018-06-06 17:31:18 +0800

    bug

--- practice/p2/main.js
+++ practice/p2/main.js
@@ -2,8 +2,9 @@ function mafiaSim(){
     localStorage.lastGame = '杀人游戏简单版';
     window.location.href =  gameMap[localStorage.lastGame];//'./mafia'
 }
-
-document.getElementById('previousText').innerHTML = '上次游戏： '+localStorage.lastGame;
+if(!localStorage.lastGame){
+    document.getElementById('previousText').innerHTML = '上次游戏： '+localStorage.lastGame;
+}
 
 const gameMap = {
     '杀人游戏简单版': './mafia'
