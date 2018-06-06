21ecd7ee40729bf63ea6ab9f4273f08216ff7271
thorzhou <thorzhou@hotmail.com>
2018-06-06 17:29:05 +0800

    responsible

---
+++ practice/p2/mafia/play/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    alert('faffea');
+})
+$('p').click(function(){
+    $(this).hide();
+})
--- practice/p2/main.js
+++ practice/p2/main.js
@@ -1,3 +1,10 @@
 function mafiaSim(){
-    window.location.href = './mafia'
+    localStorage.lastGame = '杀人游戏简单版';
+    window.location.href =  gameMap[localStorage.lastGame];//'./mafia'
+}
+
+document.getElementById('previousText').innerHTML = '上次游戏： '+localStorage.lastGame;
+
+const gameMap = {
+    '杀人游戏简单版': './mafia'
 }
